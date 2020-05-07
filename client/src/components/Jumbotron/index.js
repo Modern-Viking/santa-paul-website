4ac8b7e27613d89193daf74f8c56ca4361f8a5a0
seanianking <seanianking@gmail.com>
2020-05-06 20:29:41 -0600

    scaling text ready done

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,20 +1,44 @@
 import React from "react";
+import LightStringOne from "../gifComponents/lightStringOne";
 
 const Jumbotron = () => {
-    return(
-        <section className="hero is-primary is-medium">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">
-              Medium title
-            </h1>
-            <h2 className="subtitle">
-              Medium subtitle
-            </h2>
+  return (
+    <section className="hero is-primary is-medium">
+      <div className="hero-body">
+        <div className="container">
+
+          <div className='columns has-content-centered has-less-padding'>
+            <div className='column'>
+              <p className='jumboFontScale1'>Welcome to</p>
+            </div>
+          </div>
+
+          <div className='columns has-content-centered has-less-padding'>
+            <div className='column'>
+            <p className='jumboFontScale2'>Santa Paul's</p>
+            </div>
+          </div>
+          <div className='columns is-gapless'>
+            <div className='column'>
+              <LightStringOne />
+            </div>
+            <div className='column'>
+              <LightStringOne />
+            </div>
+            <div className='column'>
+              <LightStringOne />
+            </div>
+          </div>
+
+        <div className='columns has-content-centered has-less-padding'>
+          <div className='column'>
+            <p className='jumboFontScale1'>Workshop</p>
           </div>
         </div>
-      </section>
-    )
+      </div>
+      </div>
+    </section >
+  )
 };
 
 export default Jumbotron;
