ad155fce6f66b980f5cbe5f0b05b6026866b3e54
seanianking <seanianking@gmail.com>
2020-05-08 23:44:29 -0600

    gif icons added to buttons and sized, fonts updated as well

--- client/src/components/Buttons/Facebook.js
+++ client/src/components/Buttons/Facebook.js
@@ -1,5 +1,6 @@
 import React  from 'react';
-
+import GallopingReindeer from '../gifComponents/gallopingReindeer';
+import './buttons.css'
 
 const Facebook = () => {
 
@@ -7,10 +8,14 @@ const Facebook = () => {
         <div>
             <p className="control">
             <a className="button is-link is-focused is-full is-large" href="https://www.facebook.com/nicolaus.claus.1">
-              <span className="">
-              <i className="fab fa-facebook-square"></i>
-              <span className='is-w-95'> Santa's Facebook</span>
-              </span>
+            <div className='columns is-vcentered is-gapless'>
+            <div className='column' style={{width:'100px', height:'100px'}}>
+              <GallopingReindeer/>
+            </div>
+            <div className='column'>
+              <span className='buttons'>Santa's Facebook</span>
+            </div>
+          </div>
             </a>
           </p>
         </div>
